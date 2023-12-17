9aca14bba00f994a2ede8acfa48f7a4452138301
kyron27 <kabrah3@students.towson.edu>
2023-12-17 14:15:38 -0500

    #8 refactor: getOptionUI() in OptionUI.java
    Implemented structural improvements in getOptionUI() within OptionUI.java to enhance code readability and maintainability.
    
    Changes:
    
    -Changed structure from if-else statements to Switch cases
    
    Before
    
    public static OptionUI getOptionUI(GUI gui, Option option, boolean editable) {
    
            if (option instanceof BooleanOption) {
    
                return new BooleanOptionUI((BooleanOption)option, editable);
    
            } else if (option instanceof FileOption) {
    
                return new FileOptionUI(gui, (FileOption)option, editable);
    
            } else if (option instanceof PercentageOption) {
    
                return new PercentageOptionUI((PercentageOption)option, editable);
    
             ...
    
    After
    
        public static OptionUI<?> getOptionUI(GUI gui, Option option, boolean editable) {
    
            switch (option.getClass().getSimpleName()) {
    
                case BooleanOption:
    
                    return new BooleanOptionUI((BooleanOption) option, editable);
    
                case FileOption:
    
                    return new FileOptionUI(gui, (FileOption) option, editable);
    
                case PercentageOption:
    
                    return new PercentageOptionUI((PercentageOption) option, editable);
    
                ...
    
    Closes #8

--- FreeCol/src/net/sf/freecol/client/gui/option/OptionUI.java
+++ FreeCol/src/net/sf/freecol/client/gui/option/OptionUI.java
@@ -109,42 +109,42 @@ public abstract class OptionUI<T extends Option<?>> implements OptionUpdater {
      * @param editable Should the result be editable.
      * @return A suitable <code>OptionUI</code>, or null if none found.
      */
-    public static OptionUI getOptionUI(GUI gui, Option option, boolean editable) {
-        if (option instanceof BooleanOption) {
-            return new BooleanOptionUI((BooleanOption)option, editable);
-        } else if (option instanceof FileOption) {
-            return new FileOptionUI(gui, (FileOption)option, editable);
-        } else if (option instanceof PercentageOption) {
-            return new PercentageOptionUI((PercentageOption)option, editable);
-        } else if (option instanceof RangeOption) {
-            return new RangeOptionUI((RangeOption)option, editable);
-        } else if (option instanceof SelectOption) {
-            return new SelectOptionUI((SelectOption)option, editable);
-        } else if (option instanceof IntegerOption) {
-            return new IntegerOptionUI((IntegerOption)option, editable);
-        } else if (option instanceof StringOption) {
-            return new StringOptionUI((StringOption)option, editable);
-        } else if (option instanceof LanguageOption) {
-            return new LanguageOptionUI((LanguageOption)option, editable);
-        } else if (option instanceof AudioMixerOption) {
-            return new AudioMixerOptionUI(gui, (AudioMixerOption)option, editable);
-        } else if (option instanceof FreeColAction) {
-            return new FreeColActionUI((FreeColAction)option, editable);
-        } else if (option instanceof AbstractUnitOption) {
-            return new AbstractUnitOptionUI((AbstractUnitOption)option, editable);
-        } else if (option instanceof ModOption) {
-            return new ModOptionUI((ModOption)option, editable);
-        } else if (option instanceof UnitListOption) {
-            return new ListOptionUI<>(gui, (UnitListOption)option, editable);
-        } else if (option instanceof ModListOption) {
-            return new ListOptionUI<>(gui, (ModListOption)option, editable);
-        } else if (option instanceof TextOption) {
-            return new TextOptionUI((TextOption)option, editable);
-        } else {
-            return null;
+    public static OptionUI<?> getOptionUI(GUI gui, Option option, boolean editable) {
+        switch (option.getClass().getSimpleName()) {
+            case "BooleanOption":
+                return new BooleanOptionUI((BooleanOption) option, editable);
+            case "FileOption":
+                return new FileOptionUI(gui, (FileOption) option, editable);
+            case "PercentageOption":
+                return new PercentageOptionUI((PercentageOption) option, editable);
+            case "RangeOption":
+                return new RangeOptionUI((RangeOption) option, editable);
+            case "SelectOption":
+                return new SelectOptionUI((SelectOption) option, editable);
+            case "IntegerOption":
+                return new IntegerOptionUI((IntegerOption) option, editable);
+            case "StringOption":
+                return new StringOptionUI((StringOption) option, editable);
+            case "LanguageOption":
+                return new LanguageOptionUI((LanguageOption) option, editable);
+            case "AudioMixerOption":
+                return new AudioMixerOptionUI(gui, (AudioMixerOption) option, editable);
+            case "FreeColAction":
+                return new FreeColActionUI((FreeColAction) option, editable);
+            case "AbstractUnitOption":
+                return new AbstractUnitOptionUI((AbstractUnitOption) option, editable);
+            case "ModOption":
+                return new ModOptionUI((ModOption) option, editable);
+            case "UnitListOption":
+                return new ListOptionUI<>(gui, (UnitListOption) option, editable);
+            case "ModListOption":
+                return new ListOptionUI<>(gui, (ModListOption) option, editable);
+            case "TextOption":
+                return new TextOptionUI((TextOption) option, editable);
+            default:
+                return null;
         }
     }
-
 
     // Routines to be implemented/overridden
 
